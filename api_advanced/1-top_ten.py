786ca968b9142231c40f2be75b938f869fb82486
NickMuhigi <n.muhigi@alustudent.com>
2024-03-03 09:09:26 -0800

    S

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,38 +6,25 @@ subreddit.
 """
 import requests
 
-
 def top_ten(subreddit):
-    """Prints the titles of the first
-    10 hot posts listed for a given
-    subreddit.
-    """
-    # Set the Default URL strings
     base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/hot.json'.format(base=base_url,
-                                                     subreddit=subreddit)
-
-    # Set an User-Agent
+    api_uri = f'{base_url}/r/{subreddit}/hot.json'
     user_agent = {'User-Agent': 'Python/requests'}
-
-    # Set the Query Strings to Request
     payload = {'limit': '10'}
 
-    # Get the Response of the Reddit API
-    res = requests.get(api_uri, headers=user_agent,
-                       params=payload, allow_redirects=False)
+    res = requests.get(api_uri, headers=user_agent, params=payload, allow_redirects=False)
 
-    # Checks if the subreddit is invalid
-    if res.status_code in [302, 404]:
+    if res.status_code == 200:  # If response is successful
+        try:
+            res_json = res.json()
+            hot_posts = res_json['data']['children']
+            for post in hot_posts:
+                title = post['data']['title']
+                print(title)
+        except (KeyError, ValueError):
+            print('Error: Unable to parse JSON response')
+    elif res.status_code == 302:  # Handle redirect
         print('None')
     else:
-        res_json = res.json()
+        print(f"Error: Unexpected response code {res.status_code}")
 
-        if res_json.get('data') and res_json.get('data').get('children'):
-            # Get the 10 hot posts of the subreddit
-            hot_posts = res_json.get('data').get('children')
-
-            # Print each hot post title
-            for post in hot_posts:
-                if post.get('data') and post.get('data').get('title'):
-                    print(post.get('data').get('title'))

d18c9ae70841fcce329c0a41cfebfcefa2ab8bf2
NickMuhigi <n.muhigi@alustudent.com>
2024-03-15 08:06:40 -0700

    ZZZAQ

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,14 +2,28 @@
 """Return number of subscribers for a given subreddit"""
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Return the number of subscribers """
-    url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers)
+    
     if response.status_code == 200:
-        return response.json().get('data') \
-            .get('subscribers')
-    return 0
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
+
+# Test cases
+def test_existing_subreddit():
+    assert number_of_subscribers("python") == 757426
+
+def test_non_existing_subreddit():
+    assert number_of_subscribers("thissubredditdoesnotexist123") == 0
+
+# Module-level test runner
+if __name__ == "__main__":
+    test_existing_subreddit()
+    test_non_existing_subreddit()
+    print("All tests passed.")

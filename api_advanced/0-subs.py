b1b03805138611ac58e5df606b737ab94bb2b541
NickMuhigi <n.muhigi@alustudent.com>
2024-03-15 07:44:52 -0700

    ZZZAQ

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-""" 0-subs module """
+""" 
+This module provides a function to retrieve the number of subscribers for a given subreddit using the Reddit API.
+"""
 import requests
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:

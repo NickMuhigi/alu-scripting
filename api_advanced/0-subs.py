68afba312e9cf3a5948d71c9e92e85d9f08ec795
NickMuhigi <n.muhigi@alustudent.com>
2024-03-15 07:41:58 -0700

    ZZZAQ

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,14 +2,14 @@
 """ 0-subs module """
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """ returns the number of subscribers """
-    url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
-        return response.json().get('data') \
-            .get('subscribers')
-    return 0
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
